0b988659d405ba45eca9c8fc502f5dcc9e5b5139
Vu Hong Duong <duong.vuhong.uet@gmail.com>
2021-02-03 19:42:13 +0900

    Calendar: Update models-related login feature

--- unknown_calendar/models.py
+++ unknown_calendar/models.py
@@ -1,16 +1,32 @@
-from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class CalendarEvent(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    start_time = models.DateTimeField(default=datetime.now)
-    stop_time = models.DateTimeField(default=datetime.now)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
 class GoogleCredential(models.Model):
-    token = models.TextField()
-    refresh_token = models.TextField()
-    token_uri = models.TextField()
-    client_id = models.TextField()
-    client_secret = models.TextField()
-    scopes = models.TextField()
+    token = models.TextField(blank=True)
+    refresh_token = models.TextField(blank=True)
+    token_uri = models.TextField(blank=True)
+    client_id = models.TextField(blank=True)
+    client_secret = models.TextField(blank=True)
+    scopes = models.TextField(blank=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    creds = models.OneToOneField(GoogleCredential, null=True, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
