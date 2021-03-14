2f15f2ce755a786411986961a3ce0ad781fd2991
Vu Hong Duong <duong.vuhong.uet@gmail.com>
2021-03-15 01:29:11 +0900

    temp commit

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,46 +1,15 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from news.models import ArticleModel
-from news.serializers import ArticleSerializer
+from .models import ArticleModel
+from .serializer import ArticleSerializer
 
-@csrf_exempt
-def article_list(request):
+
+class ArticleList(APIView):
     """
-    List all code article
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         snippets = ArticleModel.objects.all()
         serializer = ArticleSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        pass
-
-@csrf_exempt
-def snippet_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        snippet = ArticleModel.objects.get(pk=pk)
-    except ArticleModel.DoesNotExist:
-        return HttpResponse(status=404)
-
-    """
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
-    """
+        return Response(serializer.data)
